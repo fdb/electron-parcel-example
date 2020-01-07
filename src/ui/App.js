c11717c9bbe08ba000dce97090c5889bdc056be8
Frederik De Bleser <frederik@debleser.be>
2020-01-07 11:51:21 +0100

    Enable quitting.

--- src/ui/App.js
+++ src/ui/App.js
@@ -1,4 +1,5 @@
 import { h, Component } from 'preact';
+const { remote, ipcRenderer } = require('electron');
 import appIcon from '../../assets/icons/app-icon-512.png';
 
 export default class App extends Component {
@@ -10,8 +11,19 @@ export default class App extends Component {
     this._onClick = this._onClick.bind(this);
   }
 
+  componentDidMount() {
+    ipcRenderer.on('menu-event', (_, { name }) => this._onMenuEvent(name));
+  }
+
   _onClick() {
     this.setState({ clicks: this.state.clicks + 1 });
+  }
+
+  _onMenuEvent(name) {
+    switch (name) {
+      case 'quit':
+        remote.app.quit();
+    }
   }
 
   render(_, { clicks }) {
